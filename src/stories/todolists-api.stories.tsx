3a4e8114186ab8a40bae10a44114246f6fddf28a
kozyrovladimir <kozyrovladimir@gmail.com>
2022-02-08 10:36:44 +0300

    fix bugs in storybook

--- src/stories/todolists-api.stories.tsx
+++ src/stories/todolists-api.stories.tsx
@@ -36,12 +36,12 @@ export const CreateTodolist = () => {
     return <div> {JSON.stringify(state)}</div>
 }
 export const DeleteTodolist = () => {
-    const todolistID = 'e0d4996c-31aa-4c26-b489-0a96ab13ca87';
+    const todolistID = '3223364a-e08c-4c98-95ce-1ba38befaf20';
     const [state, setState] = useState<any>(null)
     const settings = {
         withCredentials: true,
         headers: {
-            'API-KEY': '96ab6d46-12b3-416f-b1e5-bcbdbd4f8c3b'
+            'API-KEY': '3223364a-e08c-4c98-95ce-1ba38befaf20'
         }
     }
     useEffect(() => {
@@ -62,7 +62,7 @@ export const UpdateTodolistTitle = () => {
         }
     }
     const title = 'New name'
-    const todolistID = '96ab6d46-12b3-416f-b1e5-bcbdbd4f8c3b';
+    const todolistID = '3223364a-e08c-4c98-95ce-1ba38befaf20';
     useEffect(() => {
         todolistAPI.updateTodolist(todolistID, title)
             .then((res) => {
